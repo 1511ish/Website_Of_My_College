4b5b99ab521ce8a5ea33d5ad8bcc99a01e33fd88
Ishant Pal <mrishantsre1511ish@gmail.com>
2023-04-23 17:59:51 +0530

    frontent_website

---
+++ script.js
@@ -0,0 +1,12 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+function myFunction(){
+    alert("Your form has been summited if any information has been incorrect you can fill up the form again")
+}
